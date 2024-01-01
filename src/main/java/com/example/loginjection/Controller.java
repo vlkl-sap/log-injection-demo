15fc7ae10a63299ef1b093bcc0c1be64fa9afb90
Vladimir Klebanov <vladimir.klebanov@sap.com>
2024-01-01 18:50:41 +0100

    log request data

--- src/main/java/com/example/loginjection/Controller.java
+++ src/main/java/com/example/loginjection/Controller.java
@@ -3,12 +3,16 @@ package com.example.loginjection;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class Controller {
 
     @GetMapping("/endpoint")
     public String endpoint(@RequestParam("data") String s) {
+        Logger logger = LoggerFactory.getLogger(Controller.class);
+        logger.info(s);
         return "OK";
     }
 }
